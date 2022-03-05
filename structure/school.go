0c39e054344cc8401b631063dcd144052e79d1a1
koldin108902 <alex108902@naver.com>
2022-03-05 19:08:40 +0900

    update: school structure

--- structure/school.go
+++ structure/school.go
@@ -3,63 +3,39 @@ package structure
 // 작성 후기 시발 개 뭐라는겨
 type SchoolCode struct {
 	SchoolInfo []struct {
-		Head   []int `json:"list_total_count"`
-		Result struct {
-			Code    string `json:"CODE"`
-			Message string `json:"MESSAGE"`
-		}
+		Head []struct {
+			ListTotalCount int `json:"list_total_count,omitempty"`
+			Result         struct {
+				Code    string `json:"CODE"`
+				Message string `json:"MESSAGE"`
+			} `json:"RESULT,omitempty"`
+		} `json:"head,omitempty"`
+		Row []struct {
+			AtptOfcdcScCode       string      `json:"ATPT_OFCDC_SC_CODE"`
+			AtptOfcdcScNm         string      `json:"ATPT_OFCDC_SC_NM"`
+			SdSchulCode           string      `json:"SD_SCHUL_CODE"`
+			SchulNm               string      `json:"SCHUL_NM"`
+			EngSchulNm            string      `json:"ENG_SCHUL_NM"`
+			SchulKndScNm          string      `json:"SCHUL_KND_SC_NM"`
+			LctnScNm              string      `json:"LCTN_SC_NM"`
+			JuOrgNm               string      `json:"JU_ORG_NM"`
+			FondScNm              string      `json:"FOND_SC_NM"`
+			OrgRdnzc              string      `json:"ORG_RDNZC"`
+			OrgRdnma              string      `json:"ORG_RDNMA"`
+			OrgRdnda              string      `json:"ORG_RDNDA"`
+			OrgTelno              string      `json:"ORG_TELNO"`
+			HmpgAdres             string      `json:"HMPG_ADRES"`
+			CoeduScNm             string      `json:"COEDU_SC_NM"`
+			OrgFaxno              string      `json:"ORG_FAXNO"`
+			HsScNm                string      `json:"HS_SC_NM"`
+			IndstSpeclCccclExstYn string      `json:"INDST_SPECL_CCCCL_EXST_YN"`
+			HsGnrlBusnsScNm       string      `json:"HS_GNRL_BUSNS_SC_NM"`
+			SpclyPurpsHsOrdNm     interface{} `json:"SPCLY_PURPS_HS_ORD_NM"`
+			EneBfeSehfScNm        string      `json:"ENE_BFE_SEHF_SC_NM"`
+			DghtScNm              string      `json:"DGHT_SC_NM"`
+			FondYmd               string      `json:"FOND_YMD"`
+			FoasMemrd             string      `json:"FOAS_MEMRD"`
+			LoadDtm               string      `json:"LOAD_DTM"`
+		} `json:"row,omitempty"`
 	} `json:"schoolInfo"`
-
-	Row []struct {
-		// 교육청 코드
-		ATPT_OFCDC_SC_CODE string
-		// 교육청 이름
-		ATPT_OFCDC_SC_NM string
-		// 학교 코드
-		SD_SCHUL_CODE string
-		// 학교 이름
-		SCHUL_NM string
-		// 학교 영어 이름
-		ENG_SCHUL_NM string
-		// 초, 중, 고 ex)고등학교
-		SCHUL_KND_SC_NM string
-		// 시 도
-		LCTN_SC_NM string
-
-		JU_ORG_NM string
-		// 사립 공립
-		FOND_SC_NM string
-		// 우편번호
-		ORG_RDNZC string
-		// 주소
-		ORG_RDNMA string
-
-		ORG_RDNDA string
-		// 학교 전번
-		ORG_TELNO string
-		// 홈페이지
-		HMPG_ADRES string
-		// 학교 특성 ex) 남여공학
-		COEDU_SC_NM string
-		// 팩스 번호
-		ORG_FAXNO string
-		// ex) 특성화고
-		HS_SC_NM string
-
-		INDST_SPECL_CCCCL_EXST_YN string
-
-		// 계열 ex) 전문계
-		HS_GNRL_BUSNS_SC_NM string
-
-		SPCLY_PURPS_HS_ORD_NM string
-		ENE_BFE_SEHF_SC_NM    string
-		// 학교 시간 ex) 주간
-		DGHT_SC_NM string
-		// 개교일
-		FOND_YMD string
-		// 개교일 2
-		FOAS_MEMRD string
-		// 마지막 수정일
-		LOAD_DTM string
-	} `json:"row"`
 }
