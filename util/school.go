c7b02809f732c5d993a68e35312ed82f2e670e69
koldin108902 <alex108902@naver.com>
2022-03-05 19:49:26 +0900

    add: function's comment

--- util/school.go
+++ util/school.go
@@ -10,6 +10,7 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+// 유저 정보를 읽어와 해당 인터페이스에 파싱해줌
 func GetUserData(userInfoes *[]structure.User) error {
 	data, err := os.Open("./data/user.json")
 	if err != nil {
@@ -20,6 +21,7 @@ func GetUserData(userInfoes *[]structure.User) error {
 	return nil
 }
 
+// error processer
 func ErrProcesser(message string, err error, res *discordgo.Session, req *discordgo.MessageCreate) {
 	log := logger.Logger
 
